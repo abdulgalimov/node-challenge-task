6d7928e61a7cae00f08a3d9a07234deff5b5b529
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2025-09-27 14:06:04 +0300

    rename tokens field

--- src/modules/db/services/token.service.ts
+++ src/modules/db/services/token.service.ts
@@ -53,7 +53,7 @@ export class TokenService {
     limit: number
   ): Promise<TokenSelect[]> {
     const { rows } = await tx.execute<{ id: string }>(
-      sql`SELECT id FROM tokens WHERE price_update_required = true FOR UPDATE SKIP LOCKED LIMIT ${limit};`
+      sql`SELECT id FROM tokens WHERE wait_price_update = true FOR UPDATE SKIP LOCKED LIMIT ${limit};`
     );
 
     return Promise.all(rows.map((row) => this.getByIdSafe(row.id)));
