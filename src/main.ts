a99310b6d2bd4247e5fd4fcfc6bb8f1fe849106d
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2025-09-27 13:13:51 +0300

    use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { setupGracefulShutdown } from "nestjs-graceful-shutdown";
-import { CommonLogger, createSwagger } from "./utils";
 import { ConfigService } from "@nestjs/config";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+
+import { AppModule } from "./app.module";
+import { CommonLogger, createSwagger } from "./utils";
 
 async function bootstrap() {
   const logger = new CommonLogger("bootstrap");
   logger.log("Starting Token Price Service...");
 
   try {
-    const app = await NestFactory.create(AppModule, {
-      logger,
-    });
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+      {
+        logger,
+      }
+    );
+
     const configService = app.get(ConfigService);
 
     createSwagger(app);
